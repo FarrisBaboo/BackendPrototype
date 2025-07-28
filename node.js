c56688d1b644d2d3c30bc2d18e02c706441d1aa0
FarrisBaboo <fbaboo0111@gmail.com>
2025-07-28 13:27:07 +1000

    Add files via upload

---
+++ node.js
@@ -0,0 +1,16 @@
+// index.js
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Example route
+app.get('/', (req, res) => {
+  res.send('Backend is running');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
